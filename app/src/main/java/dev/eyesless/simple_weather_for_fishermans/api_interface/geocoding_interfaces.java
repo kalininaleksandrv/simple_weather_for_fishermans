1a4b4d45856e95081bc87f060ed005a12aecfd4c
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2017-08-08 14:15:48 +0300

    faeture - add @Query to retrofit and logger to OkHttp

--- app/src/main/java/dev/eyesless/simple_weather_for_fishermans/api_interface/geocoding_interfaces.java
+++ app/src/main/java/dev/eyesless/simple_weather_for_fishermans/api_interface/geocoding_interfaces.java
@@ -2,21 +2,19 @@ package dev.eyesless.simple_weather_for_fishermans.api_interface;
 
 import dev.eyesless.simple_weather_for_fishermans.geocoding_responce_classes.Geocod;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface geocoding_interfaces {
 
-    String BASE_URL = "https://maps.googleapis.com/maps/api/geocode/";
-    String PRIVATE_KEY = "";
+    String BASE_URL = "https://maps.googleapis.com/maps/api/";
 
-
-    OkHttpClient CLIENT = new OkHttpClient();
-
-    @GET("json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key="+PRIVATE_KEY)
-    Call<Geocod> getCoordinates();
+    @GET("geocode/json")
+    Call<Geocod> getCoordinates(@Query("address") String order, @Query("key") String key);
 
     class CoordinatesFactory {
 
@@ -26,10 +24,16 @@ public interface geocoding_interfaces {
 
             if (service == null) {
 
+                HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+                logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
+                OkHttpClient client = new OkHttpClient.Builder()
+                        .addInterceptor(logging)
+                        .build();
+
                 Retrofit retrofit = new Retrofit.Builder()
                         .baseUrl(BASE_URL)
                         .addConverterFactory(GsonConverterFactory.create())
-                        .client(CLIENT)
+                        .client(client)
                         .build();
 
                 service = retrofit.create(geocoding_interfaces.class);
