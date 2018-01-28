aaceef2fc71f9dc230a1938d8964ee542e1c7564
Aleksandr Kalinin <kalinin.aleksandr.v@gmail.com>
2018-01-28 14:37:02 +0300

    feature - add presure to prognostic model

--- app/src/main/java/dev/eyesless/simple_weather_for_fishermans/repository/PrognosticModel.java
+++ app/src/main/java/dev/eyesless/simple_weather_for_fishermans/repository/PrognosticModel.java
@@ -14,12 +14,12 @@ import static java.lang.Math.max;
 public class PrognosticModel {
 
 
-    private final static int PROG_TEMPRETURE_MAX_OPTIMUM = 23;
-    private final static int PROG_TEMPRETURE_MIN_OPTIMUM = -4;
-    private final static int PROG_TEMPRETURE_MAX_WORSE = 29;
-    private final static int PROG_TEMPRETURE_MIN_WORSE = -9;
-    private final static int PROG_TEMPRETURE_MAX_BAD = 36;
-    private final static int PROG_TEMPRETURE_MIN_BAD = -19;
+    private final static int PROG_TEMPRETURE_MAX_OPTIMUM = 18;
+    private final static int PROG_TEMPRETURE_MIN_OPTIMUM = -7;
+    private final static int PROG_TEMPRETURE_MAX_WORSE = 24;
+    private final static int PROG_TEMPRETURE_MIN_WORSE = -14;
+    private final static int PROG_TEMPRETURE_MAX_BAD = 30;
+    private final static int PROG_TEMPRETURE_MIN_BAD = -22;
 
     private final static int PROGNOSE_DEPTH = 5;
     private final static int DAYS_COUNT = 7;
@@ -29,6 +29,7 @@ public class PrognosticModel {
 
     private final double[] maxtemparray;
     private final double[] mintemparray;
+    private final static double PRESSUREDEVIATION = 0.008; //officially changing pressure more than 8% defined as "sharp change"
 
     public PrognosticModel(List<Datum> incomedata) {
         this.incomedata = incomedata;
@@ -72,6 +73,8 @@ public class PrognosticModel {
 
         String  [] dayEstimate = new String [DAYS_COUNT];
 
+        int pressureAccumulatedEffect = 0;
+
         for (int j = 0; j < DAYS_COUNT; j++) {
 
             int optimumcounter = 0;
@@ -81,12 +84,16 @@ public class PrognosticModel {
 
             int [] temperatureArray = new int[5];
 
+            double [] pressureArray = new double[5];
 
+            //filling arrays by data
             for (int i = 0; i < PROGNOSE_DEPTH; i++) {
 
                 temperatureArray[i] = isTempretureGood(incomedata.get(startingday+i).getTemperatureMin(), incomedata.get(startingday+i).getTemperatureMax());
-
+                pressureArray [i] = incomedata.get(startingday+i).getPressure();
             }
+
+            //effect of temperature
             for (int i=0; i<5; i++){
 
                 if (Math.abs(temperatureArray[i]) == 3){
@@ -102,6 +109,63 @@ public class PrognosticModel {
                 }
             }
 
+            //effect of fast normalising of temperature
+            if (temperatureArray[4]<=1){
+
+                int temp = temperatureArray[4];
+
+                if ((temperatureArray[3]-temp)>=2 && (temperatureArray[2]-temp)>=2 && (temperatureArray[1]-temp)>=2 && (temperatureArray[0]-temp)>=2){
+
+                    optimumcounter++;
+                    Log.i("MY_TAG", "Effect of fast normalising: last day = " + String.valueOf(startingday) + "previous days = " + String.valueOf(temperatureArray[3])+ String.valueOf(temperatureArray[2])+ String.valueOf(temperatureArray[1])+ String.valueOf(temperatureArray[0]));
+
+                }
+            }
+
+            //effect of fast pressure change
+            int isPressureDeviant = 0;
+
+            //here we're count how many times difference between today and yesterday pressure was >X% and increase var. isDeviantPressure, X = PRESSUREDEVIATION
+            for (int i = 4; i>0; i--){
+                double currentPressure = pressureArray[i];
+                double currentPressureDeviation = currentPressure * PRESSUREDEVIATION;
+                double previouspressure = pressureArray [i-1];
+
+                if (Math.abs(currentPressure-previouspressure)>currentPressureDeviation){
+                    isPressureDeviant++;
+                }
+
+            }
+            // here we're check - how many times pressure was deviant strongly and decreasing forecast depend of that
+            switch (isPressureDeviant) {
+                case 0 :
+                    pressureAccumulatedEffect--; //accumulated effect using in final count (look below) - if pressure effect was bad 3 days in a row, so we inc and dec this war each cycle
+                    break;
+                case 1 :
+                    pressureAccumulatedEffect--;
+                    break;
+                case 2 :
+                    optimumcounter--;
+                    worsecounter++;
+                    Log.i("MY_TAG", "Pressure WORSE");
+                    break;
+                case 3 :
+                    optimumcounter-=2;
+                    worsecounter ++;
+                    badcounter ++;
+                    pressureAccumulatedEffect++;
+                    Log.i("MY_TAG", "Pressure BAD");
+                    break;
+                case 4 :
+                    optimumcounter-=3;
+                    worsecounter ++;
+                    badcounter+=3;
+                    disastercounter++;
+                    pressureAccumulatedEffect++;
+                    Log.i("MY_TAG", "Pressure DISASTER");
+                    break;
+            }
+
 
             Log.i("MY_TAG", "Starting day is: " + String.valueOf(startingday));
             Log.i("MY_TAG", "Array is: " + Arrays.toString(temperatureArray));
@@ -110,17 +174,25 @@ public class PrognosticModel {
                     + " worse = " + String.valueOf(worsecounter)
                     + " optimum = " + String.valueOf(optimumcounter));
 
-            if ( max(optimumcounter, worsecounter) > max(badcounter, disastercounter)){
+            if (pressureAccumulatedEffect<3) {
+                if ( max(optimumcounter, worsecounter) > max(badcounter, disastercounter)){
 
-                if (optimumcounter > worsecounter){
-                    dayEstimate[j]="good";
+                    if (optimumcounter > worsecounter){
+                        dayEstimate[j]="good";
+                    } else {
+                        dayEstimate[j]="average";
+                    }
+                } else if (badcounter > disastercounter) {
+                    dayEstimate[j]="downward";
                 } else {
-                    dayEstimate[j]="average";
+                    dayEstimate[j]="bad";
                 }
-            } else if (badcounter > disastercounter) {
-                dayEstimate[j]="downward";
             } else {
-                dayEstimate[j]="bad";
+                //if pressure changing fast 3 and more days bite will be bad despite other effects
+                if (pressureAccumulatedEffect>=3){
+                        dayEstimate[j]="bad";
+                        Log.i("MY_TAG", "Pressure ACCUM BAD");
+                }
             }
 
             startingday++;
